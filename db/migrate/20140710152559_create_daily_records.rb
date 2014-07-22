91b2a97592f72ce474c387ed9c6587f0e106ad9f
skylar <skylar.bolton@gmail.com>
2014-07-22 18:57:00 +0000

    Migrations testing and OOF image.

---
+++ db/migrate/20140710152559_create_daily_records.rb
@@ -0,0 +1,17 @@
+class CreateDailyRecords < ActiveRecord::Migration
+  def change
+    create_table :daily_records do |t|
+      t.date :daily_date
+      t.integer :temp_low
+      t.integer :temp_high
+      t.integer :eggs_collected
+      t.integer :total_ducks
+      t.time :hours_of_daylight
+      t.time :sunrise
+      t.integer :moon_phase_id
+      t.integer :moon_percent
+      t.text :weather
+      t.boolean :rained
+    end
+  end
+end
